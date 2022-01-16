56364f09f616501735cae029d6b3a4609c56f66a
Farhad <alliance_avto.abd@inbox.ru>
2022-01-16 17:50:54 +0600

    add slider

---
+++ js/script.js
@@ -0,0 +1,16 @@
+AOS.init();
+
+// $(document).ready(function(){
+// 	$(".owl-carousel").owlCarousel({
+// 		items:1,
+// 		loop:true,
+// 		center: true
+// 	});
+// });
+
+$('.single-item').slick({
+	infinite: true,
+	dots: true,
+	slidesToShow: 2,
+	slidesToScroll: 2,
+});
